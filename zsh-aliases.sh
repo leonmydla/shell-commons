44666c012d67e0e32b1f2c8d796af2ea95a9b2e9
Leon Mydla <leon.mydla@alphabrik.com>
2020-04-03 15:28:10 +0200

    additional docker aliases

--- zsh-aliases.sh
+++ zsh-aliases.sh
@@ -19,7 +19,10 @@ alias dps="docker ps"
 alias dpsa="docker ps -a"
 
 alias dr="docker run"
+alias drrm="docker run --rm"
+alias drit="docker run -it"
 alias drrmit="docker run --rm -it"
+alias drd="docker run -d"
 alias drrmd="docker run --rm -d"
 alias drit="docker run -it"
 
